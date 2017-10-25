dd1147b324e17e225d736edd91b3d0c76e7373a7
Administrator <Administrator@hhw>
2017-10-25 16:10:53 +0800

    分块下载

--- src/tcp/Tcp_Server.java
+++ src/tcp/Tcp_Server.java
@@ -4,6 +4,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.Date;
+import java.util.Timer;
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.io.PrintWriter;
   
 import dfu.dfuThread;
 
-public class Tcp_Server {
+public class Tcp_Server extends Thread{
 	
     //public static final String SERVER_ADDR = "192.168.1.133";
     public static final String SERVER_PORT = "5000";
@@ -30,7 +31,7 @@ public class Tcp_Server {
      */
     public static void main(String[] args) throws InterruptedException, IOException {
     	Object lock = new Object();
-    	System.out.println('3'+'4');System.out.println("3"+"4");
+    	//System.out.println('3'+'4');System.out.println("3"+"4");
     	Tcp_Server server = new Tcp_Server(lock);
     	server.method();
     }
@@ -46,6 +47,33 @@ public class Tcp_Server {
     public Tcp_Server(Object lock) 
     {
         this.dfu_lock = lock;
+    }
+    
+    public void end()
+    {
+  	    
+    }
+    
+    public void run()
+    {
+    	
+    	
+    	
+    }
+    
+    public void Thread_create(String path)
+    {
+		dfu = new dfuThread(this.dfu_lock,path);
+		dfu.start();
+		
+		//启动线程，等待100ms后，开始发送数据
+		try {
+			dfu.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		dfu.method_notify();
     }
     
     public void method() throws InterruptedException,IOException
@@ -68,61 +96,78 @@ public class Tcp_Server {
             String temp = null;
             
             while((temp = bufferedReader.readLine()) != null){
-            	
-            	//关闭服务器命令
+            	  
+            	//文件发送完毕
             	if(temp.equals("dfu_end")){
+            		System.out.println("文件结束: " + new Date());
+            		//break; //先不要退出，准备发送后续的文件
+            	}
+            	//关闭服务器命令
+            	if(temp.equals("dfu_stop")){
+            		System.out.println("下载结束: " + new Date());
+            		Thread.sleep(100);
             		break;
             	}
-            		
+            	
             	//接收到dfu开始发送的命令，启动线程
             	if(temp.equals("dfu_send"))
             	{
-            		dfu = new dfuThread(this.dfu_lock);
-            		dfu.start();
-            		
-            		//启动线程，等待100ms后，开始发送数据
-            		dfu.sleep(100);
-            		dfu.method_notify();
+            		Thread_create("C:/dfu/no-key/save/image_sink.dfu");
+            	}
+            	
+            	//接收到dfu_send1开始发送第二个DFU文件，启动线程
+            	if(temp.equals("dfu_send1"))
+            	{
+            		Thread.sleep(1000);//等上一个线程完全结束  2017-10-19
+            		Thread_create("C:/dfu/no-key/save/sink.dfu");
+            	}
+            	//接收到dfu_send2开始发送第三个DFU文件，启动线程
+            	if(temp.equals("dfu_send2"))
+            	{
+            		Thread.sleep(1000);
+            		Thread_create("C:/dfu/no-key/save/sink.dfu");
             	}
             	
             	//接收到dfu继续命令
             	if(temp.equals("dfu_go"))
             	{
+
             		dfu.method_notify();
             		if(dfu.send_ok == 1)
             		{
-            			rcv_dfu_go++;
+            			dfu.send_ok = 0;//rcv_dfu_go++;
             			System.out.println(rcv_dfu_go);
-            			if(rcv_dfu_go == 2)
-            			 {
-            				break;
-            			 }
+            			
+            			OutputStream outputStream = socket.getOutputStream();
+            			byte[] tempchars = new byte[10];
+            	        //文件传输完成后，发送一串特殊数据
+            	  	    for(int i = 0; i < 10; i++){
+            	  	    	tempchars[i] = (byte) 0xAA;
+            		    }
+            	  	    outputStream.write(tempchars,0,10);
+            	  	    System.out.println("end");
             		}
             	}
             	
                 System.out.println("信息："+temp);
+                /*
+    			if(rcv_dfu_go == 2)
+	   			 {
+    				rcv_dfu_go = 0;
+    				dfu.send_ok = 0;
+	   				//break;
+	   			 }
+	   			 */
             }
-            
-            /*
-            OutputStream outputStream = socket.getOutputStream();//获取一个输出流，向客户端发送信息
-            PrintWriter printWriter = new PrintWriter(outputStream);//将输出流包装成打印流
-            printWriter.print("你好，服务端已关闭");
-            printWriter.flush();
-            socket.shutdownOutput();//关闭输出流
-            
-            //关闭相对应的资源
-            printWriter.close();
-            outputStream.close();
-            */
+
             bufferedReader.close();
             inputStream.close();
             socket.close();
+            System.out.println("socket close !");
             
         } catch (IOException e) {
-        	System.out.println(new Date());
         	socket.close();
-            e.printStackTrace();
-                        
+            e.printStackTrace();         
         }
     } 
 }
