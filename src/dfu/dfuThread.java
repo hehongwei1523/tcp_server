7d15b6612adad926f5f75189bc4ab8a5500e8b2f
Administrator <Administrator@hhw>
2017-10-25 16:12:01 +0800

    分块下载

--- src/dfu/dfuThread.java
+++ src/dfu/dfuThread.java
@@ -11,16 +11,21 @@ import java.util.Date;
 
 import tcp.Tcp_Server;
 
+
 public class dfuThread extends Thread{
 	
+	final int FILE_LONG = 1023;//1000;
 	private Object lock;
-    public static final String dfu_file_path = "C:/dfu/app40.dfu"; //"C:/dfu/no-key/save/dump1.dfu"; //"C:/dfu/no-key/save/headset.dfu";  // 
+    //public static final String dfu_file_path = "C:/dfu/no-key/save/sink.dfu";
+    		// //"C:/dfu/app40.dfu"; // "C:/dfu/no-key/combined.dfu";  //
     
+	private String dfu_file_path;
     public static int send_ok = 0; 
-	public dfuThread(Object lock){
+	public dfuThread(Object lock,String path){
 		//构造函数
 		super();
-		this.lock = lock;		
+		this.dfu_file_path = path;
+		this.lock = lock;
 	}
 	
 	@Override
@@ -29,6 +34,7 @@ public class dfuThread extends Thread{
 		
 		try{
 			File file=new File(dfu_file_path);
+			//System.out.println(file.length());
 			
 			if(!file.exists()||file.isDirectory())
 	            throw new FileNotFoundException();
@@ -39,47 +45,62 @@ public class dfuThread extends Thread{
 			try {
 	            System.out.println("DataInputStream：");
 	            // 一次读多个数据
-	            byte[] tempchars = new byte[1023];
+	            byte[] tempchars = new byte[FILE_LONG];
+	            byte[] indexchars = new byte[FILE_LONG+10];
 	            int charread = 0;
 	            int length = 0;
 	            int times = 0;
+	            
+	            indexchars[0] = 'F';
+	            indexchars[1] = 'I';
+	            indexchars[2] = 'L';
+	            indexchars[3] = 'E';
+	            
+	            indexchars[8] = 'C';
+	            indexchars[9] = 'R';
+
+	            System.out.println(new Date());
 	            // 读入多个数据到数组中，charread为一次读取字符数
 	            while ((charread = read.read(tempchars)) != -1) {
 	
+	            	    //当检测到读到的数组长度不等于1023时，表示为最后一块。去除后缀。
+	            	    if(charread < tempchars.length)
+	            	     {
+	            	    	//charread = charread - 16;
+	            	    	/*//难道是因为这里耗时太久，导致notify没执行？ 2017-9-6
+	            	    	System.out.printf("tempchars[%d]:%x \n", 0 , tempchars[0]);
+	            	    	System.out.printf("tempchars[%d]:%x \n", 1 , tempchars[1]);
+	            	    	System.out.printf("tempchars[%d]:%x \n", charread-2 , tempchars[charread-2]);
+	            	    	System.out.printf("tempchars[%d]:%x \n", charread-1 , tempchars[charread-1]);
+	            	    	*/
+	            	     }
 	            		method_wait();
-	            		
-		              if (charread == tempchars.length) {
-		            	  //每次发送一整个数组
-		            	  length = length + tempchars.length;
-		            	  outputStream.write(tempchars);
-		            	  outputStream.flush();
-		              }
-		              else{
-		            	  //最后一次发送剩余的数据
-		            	  length = length + charread;
-		            	  for(int i = 0; i < charread; i++){
-		            		  outputStream.write(tempchars[i]);
-		            		  outputStream.flush();
-		            	  }
-		              }
+	            		//System.out.println("wait");
+	            		//dfu数据发送次数
+	            		indexchars[4] = (byte) ((times&0xff00)>>8);
+	            		indexchars[5] = (byte) (times&0x00ff);
+		            	indexchars[6] = (byte) ((charread&0xff00)>>8);
+		            	indexchars[7] = (byte) (charread&0x00ff);
+                        /*
+		            	src:源数组；	srcPos:源数组要复制的起始位置；
+		            	dest:目的数组；	destPos:目的数组放置的起始位置；	length:复制的长度。
+		            	*/
+		            	System.arraycopy(tempchars, 0, indexchars, 10, charread);
+		            	
+		            	length = length + charread;
+		              
+		            	outputStream.write(indexchars,0,charread+10);
+		            	outputStream.flush();
+		            	  
+		              
 		              times++;
-		              System.out.println("发送次数： " + times + "长度： " + charread);
+		              System.out.println("发送次数： " + times + " 长度： " + charread);
 	            }
 	            System.out.println( "文件长度length为:"+ length);
-	            outputStream.flush();
+	            //outputStream.flush();
 	            
 	            send_ok = 1;
-	            /*
-	            System.out.println(new Date());
-	            this.sleep(3000);
-	            System.out.println(new Date());
-	            //文件传输完成后，发送一串特殊数据
-          	    for(int i = 0; i < 4; i++){
-        		  outputStream.write(0xaa);
-        	    }
-          	    */
-	            //Tcp_Server.get_Socket().close(); 
-	            
+
 			}catch (Exception e1) {
 	            e1.printStackTrace();
 	        } finally {
@@ -103,13 +124,6 @@ public class dfuThread extends Thread{
 	public void method_wait() throws IOException{
 		synchronized (this.lock) {
 			try {
-				/*
-            	if(System.currentTimeMillis() - start_time > 20000)
-            	{
-            		System.out.println("timeout socket close!");
-            		Tcp_Server.get_Socket().close();
-            	}
-            	*/
         		//System.out.println("begin wait");				
 				this.lock.wait();
 				//System.out.println("wait end");
